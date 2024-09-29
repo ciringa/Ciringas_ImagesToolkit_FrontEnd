131aeba0a0d10786e8feab1b76f8763ea517efcf
ciringa <thierrird957468@gmail.com>
2024-09-28 23:25:11 -0300

    updates:responsivity changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react"
 import AppyEffect from "./components/ApplyEffect"
 import RemBgCard from "./components/RemBgCard"
 import "./styles/app.css"
 
+enum Test{
+   remBg="remBg",
+   applyEffect="applyEffect",
+}
 function App() {
+  const [SiteState,SetSiteState] = useState<Test>(Test.remBg)
+  
+  
+
   return (
-
+    
     <div id="Main">
-      <RemBgCard />
-      <AppyEffect />
+      <div id="header">
+        <button onClick={()=>{SetSiteState(Test.remBg)}}>Remover o background</button>
+        <button onClick={()=>{SetSiteState(Test.applyEffect)}}>Aplicar Efeitos</button>
+      </div>
 
+      <div>
+        {SiteState==Test.remBg?(<RemBgCard />):(<AppyEffect />)}
+      </div>
     </div>
   )
 }
